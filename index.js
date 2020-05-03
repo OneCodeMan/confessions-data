315e5cccfafb91f1d1b5ccef46e9a360d102315e
Dave Gumba <dave_gumba@Yahoo.com>
2020-05-03 08:01:12 -0400

    the functionality is complete. it's the styling now.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-alert("yo");
+// unique labels:   ["None", "Sex", "Mental Health", "Money/Financial", "Medical", "Drugs", 
+//                  "Race/Protected Groups", "Excretions", "Academics", "Death"]
+
+let uniqueLabels = [...new Set(confessions.map(item => item.label))];
+uniqueLabels.push('Random');
+
+let categorizedConfessionsDict = {};
+let textContentEl = document.getElementById('text-content');
+let buttonSelectEl = document.getElementById('button-select');
+
+for (label of uniqueLabels) {
+    let labeledConfessions = label === 'Random' ? confessions : confessions.filter(item => item.label == label);
+    categorizedConfessionsDict[label] = labeledConfessions;
+    buttonSelectEl.innerHTML += '<div class="button"><button onClick="generateConfession(value)" value="' + label + '">' + label + '</button></div>';
+}
+
+textContentEl.innerText = confessions[generateRandomIndexFromArray(confessions)].text;
+
+function generateConfession(label) {
+    let categoryList = categorizedConfessionsDict[label];
+    let randomConfession = categoryList[generateRandomIndexFromArray(categoryList)].text;
+    textContentEl.innerText = randomConfession;
+}
+
+function generateRandomIndexFromArray(array) {
+    return array.length * Math.random() | 0;
+}
+
